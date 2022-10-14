d92b333f1cbf278b81f7a8e963a4f382cb3854e8
Alamgir Khan <k.alamgir2002@gmail.com>
2022-10-14 03:58:28 -0400

    added jest test for cart drawer

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
+import { QueryClient, QueryClientProvider } from 'react-query'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('clicking shopping cart icon opens the cart drawer', async () => {
+  const client = new QueryClient();
+
+  render(<QueryClientProvider client={client}>
+    <App />
+  </QueryClientProvider>);
+
+  fireEvent.click(await screen.findByTestId("AddShoppingCartIcon"))
+  expect(screen.getByText("Shopping Cart"))
+
 });
